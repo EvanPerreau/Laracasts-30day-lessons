e04e36e8d911204ea63703a32c5b73de0a1d5aeb
EvanPerreau <evan.perreau@gmail.com>
2024-07-28 15:38:18 +0200

    lesson 6

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,52 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$100,000',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Manager',
+                'salary' => '$80,000',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Employee',
+                'salary' => '$50,000',
+            ]
+        ],
+    ]);
+});
+
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$100,000',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Manager',
+            'salary' => '$80,000',
+        ],
+        [
+            'id' => 3,
+            'title' => 'Employee',
+            'salary' => '$50,000',
+        ]
+    ];
+    $job = collect($jobs)->firstWhere('id', $id);
+
+    return view('job', [
+        'job' => $job,
+    ]);
 });
 
 Route::get('/contact', function () {
