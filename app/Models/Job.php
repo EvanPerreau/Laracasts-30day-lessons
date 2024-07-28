00e0de8e2a5cc40d05543ab7770cca9a1aac8405
EvanPerreau <evan.perreau@gmail.com>
2024-07-28 19:31:38 +0200

    lesson 9

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,32 +2,13 @@
 
 namespace App\Models;
 
-class Job {
-    public static function all(): array {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$100,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Manager',
-                'salary' => '$80,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Employee',
-                'salary' => '$50,000',
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id): array {
-        $job = collect(self::all())->firstWhere('id', $id);
-        if($job == null) {
-            abort(404);
-        }
-        return $job;
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
+
+    protected $fillable = [
+        'title',
+        'salary',
+    ];
 }
